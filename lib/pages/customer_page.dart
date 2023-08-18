54e2d105bb7f75c61e0286e6ad56794049cd832e
kezia2909 <c14190048@john.petra.ac.id>
2023-08-18 21:29:21 +0700

    desain customer & add

--- lib/pages/customer_page.dart
+++ lib/pages/customer_page.dart
@@ -3,6 +3,7 @@ import 'package:food_sales_recording_application/widgets/detail_text.dart';
 
 import '../utils/app_colors.dart';
 import '../widgets/children_detail_item_history.dart';
+import '../widgets/currency_input_formatter.dart';
 import '../widgets/title_item_history.dart';
 import '../widgets/title_text.dart';
 import '../widgets/trailing_item_history.dart';
@@ -16,6 +17,8 @@ class CustomerPage extends StatefulWidget {
 
 class _CustomerPageState extends State<CustomerPage> {
   final isExpandedList = List<bool>.generate(10, (int index) => false);
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +36,68 @@ class _CustomerPageState extends State<CustomerPage> {
               isBold: true,
             ),
           ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            child: Column(children: [
+              TextField(
+                controller: _nameController,
+                decoration: new InputDecoration(
+                  hintText: 'John Doe',
+                  labelText: "Customer Name",
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  border: new OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(
+                      const Radius.circular(8),
+                    ),
+                    borderSide: new BorderSide(
+                      color: Colors.black,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                  controller: _addressController,
+                  decoration: new InputDecoration(
+                    hintText: 'Street No 10',
+                    labelText: "Customer Address",
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                    border: new OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(8),
+                      ),
+                      borderSide: new BorderSide(
+                        color: Colors.black,
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  inputFormatters: [CurrencyInputFormatter()],
+                  keyboardType: TextInputType.numberWithOptions(
+                      signed: false, decimal: false)),
+              SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: () => (),
+                child: Container(
+                  width: double.maxFinite,
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Appcolors.darkColor,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: TitleText(text: "Add Customer"),
+                ),
+              ),
+            ]),
+          ),
+          Divider(),
           Expanded(
             child: Container(
               color: Colors.white,
